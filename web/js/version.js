a693e0343272992e6cd66a8c0faa5217fa4f7275
郭之存 <guoplc@163.com>
2026-08-14 11:48:09 +0800

    chore(release): v2.0.0

--- web/js/version.js
+++ web/js/version.js
@@ -1,4 +1,4 @@
 // 由 tools/sync-version.mjs 自动生成，请勿手动修改
-export const VERSION = '1.1.0';
-export const BUILD_DATE = '2026-08-13';
-export const COMMIT = 'd53d5f7';
+export const VERSION = '2.0.0';
+export const BUILD_DATE = '2026-08-14';
+export const COMMIT = 'a6e0b5e';
